a557b0426d16aac30f01701c307cdcc8ff1f7ff1
brunandrade <bruna.card.andrade@gmail.com>
2019-10-05 11:45:24 -0300

    Finalização da aplicação Web

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+mongoose.connect('mongodb+srv://hansolo:hansolo@aircnc-ktep2.mongodb.net/aircnc?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..','uploads')));
+app.use(routes);
+
+app.listen(3333);
